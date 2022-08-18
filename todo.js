8261da59ff284a2f251560d8f8d206a866697b9c
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-18 12:39:45 +0600

    almose done . but clear button not working

--- todo.js
+++ todo.js
@@ -4,12 +4,13 @@ document.getElementById('button-addon2').addEventListener('click', function () {
     count++
     const mainTable = document.getElementById("table")
     const tableElement = document.createElement('tr')
+    // tableElement.classList.add(' ')
     tableElement.innerHTML = `
-    <th class="text-center" scope="row">${count}</th>
+    <td class="text-center ">${count}</td>
     <td class="text-center">${inputText}</td>
     <td class="text-center">
-    <button type="button" class="btn btn-danger bg-danger delete-btn">Remove</button>
-    <button type="button" class="btn btn-success bg-success">Done</button>
+    <button type="button" class="btn btn-danger bg-danger my-1 py-1 delete-btn">Remove</button>
+    <button type="button" class="btn btn-success done py-1 my-1 bg-success">Done</button>
     </td>`
     if(inputText!=''){
        mainTable.appendChild(tableElement)
@@ -26,7 +27,24 @@ document.getElementById('button-addon2').addEventListener('click', function () {
         })
         
     }
-})
+    const doneBtn = document.getElementsByClassName('done')
+    for(const done of doneBtn){
+        done.addEventListener('click', function(event){
+           event.target.parentNode.previousElementSibling.style.textDecoration = "line-through"
+        //    event.target.parentNode.parentNode.style.textDecoration = "line-through" 
+        })
+        
+    }
 
+
+       
+})
+document.getElementById('clear-all').addEventListener('click', function(event){
+    // console.log(event.target.previousElementSibling.chid)
+    // console.log(event.target.parentNode.childNodes[1].childNodes[3].style.display= "none")
+    document.getElementById('content-container').classList.add('d-none')
+    // console.log(table)
+
+})
 
 
